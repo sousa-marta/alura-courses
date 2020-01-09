e03587a3a3e6ee253165911fc65bbc1d032bc305
Marta Sousa <marta.sousa@gmail.com>
2020-01-09 19:14:15 -0300

    creating form for adding new patient

--- js-introduction/js/script.js
+++ js-introduction/js/script.js
@@ -1,24 +1,34 @@
-var pageTitle = document.querySelector('#title')
-pageTitle.textContent = "Durga Nutrição"
+let companyTitle = "Durga Nutrição"
+document.querySelector('#title').textContent = companyTitle
+document.querySelector('title').textContent = companyTitle
+
 
 //Pegando informações do paciente e nela selecionando dados que quero:
-var paciente = document.querySelector('#primeiro-paciente')
-var peso = paciente.querySelector('.info-peso').textContent
-var altura = paciente.querySelector('.info-altura').textContent
-var imcTabela = paciente.querySelector('.info-imc')
+var pacientes = document.querySelectorAll('.paciente')
 
-//Validações
-if (peso <= 0 || peso >= 1000 && altura >= 3){
-  imcTabela.textContent = "Peso e altura inválidos!"
-} else if (peso <= 0 || peso >= 1000) {
-  imcTabela.textContent = "Peso inválido"
-} else if (altura >= 3) {
-  imcTabela.textContent = "Altura inválida"
-} else {
-  // Se passar nas validações, calcula IMC
-  var imc = peso/(altura*altura)
-  imcTabela.textContent = imc
+for (let i = 0; i < pacientes.length; i++) {
+  paciente = pacientes[i]
+  var peso = paciente.querySelector('.info-peso').textContent
+  var altura = paciente.querySelector('.info-altura').textContent
+  var imcTabela = paciente.querySelector('.info-imc')
+  
+  //Validações
+  if (peso <= 0 || peso >= 1000 && altura >= 3){
+    imcTabela.textContent = "Peso e altura inválidos!"
+    paciente.classList.add('paciente-invalido')
+  } else if (peso <= 0 || peso >= 1000) {
+    imcTabela.textContent = "Peso inválido"
+    paciente.classList.add('paciente-invalido')
+  } else if (altura >= 3) {
+    imcTabela.textContent = "Altura inválida"
+    paciente.classList.add('paciente-invalido')
+  } else {
+    // Se passar nas validações, calcula IMC
+    var imc = peso/(altura*altura)
+    imcTabela.textContent = imc.toFixed(2)
+  }
 }
+
 
 
 
